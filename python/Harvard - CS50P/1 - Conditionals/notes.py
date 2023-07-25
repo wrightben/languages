431880a329f9fe8633b64a2f1463bc19a3567051
Benjamin Wright <wrightben@Benjamins-MacBook-Pro.local>
2023-07-25 12:43:14 -0400

    gitall commit

--- python/Harvard - CS50P/1 - Conditionals/notes.py
+++ python/Harvard - CS50P/1 - Conditionals/notes.py
@@ -5,5 +5,11 @@ if x < y:
 	print("x is less than y")
 elif x > y:
 	print("x is greater than y")
-elif x == y:
+else:
+	print("x is equal to y")
+	
+	
+if (x < y) or (x > y):
+	print("x is not equal to y")
+else:
 	print("x is equal to y")
