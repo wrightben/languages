72932cd886b22d461fabb7d6d32fb90eabd9bf51
Benjamin Wright <wrightben@Benjamins-MacBook-Pro.local>
2023-07-25 15:34:45 -0400

    gitall commit

--- python/Harvard - CS50P/1 - Conditionals/notes.py
+++ python/Harvard - CS50P/1 - Conditionals/notes.py
@@ -1,15 +1,68 @@
-x = int(input("What is x? "))
-y = int(input("What is y? "))
+def example1():
+	x = int(input("What is x? "))
+	y = int(input("What is y? "))
 
-if x < y:
-	print("x is less than y")
-elif x > y:
-	print("x is greater than y")
-else:
-	print("x is equal to y")
+	# if <, elif >, else
+	if x < y:
+		print("x is less than y")
+	elif x > y:
+		print("x is greater than y")
+	else:
+		print("x is equal to y")
 	
+	# if-or, else
+	if (x < y) or (x > y):
+		print("x is not equal to y")
+	else:
+		print("x is equal to y")
+		
+#example1()
+
+
+
+def example2():
+	score = int(input("Score: "))
 	
-if (x < y) or (x > y):
-	print("x is not equal to y")
-else:
-	print("x is equal to y")
+							# Optimized
+	if 90 <= score <= 100:	# if score >= 90:
+		print("Grade: A")
+	elif 80 <= score < 90:
+		print("Grade: B")
+	elif 70 <= score < 80:
+		print("Grade: C")
+	elif 60 <= score < 70:
+		print("Grade: D")
+	else:
+		print("Grade: F")
+		
+#example2()
+
+
+
+def is_even(n):
+	# return True if n % 2 == 0 else False
+	return n % 2 == 0
+
+def example3():
+	x = int(input("What is x? "))
+
+	if is_even(x):
+		print("Even")
+	else:
+		print("Odd")
+
+#example3()
+
+
+
+
+def example4():  
+	name = input("What's your name? ")
+
+	match name: # Requires Python 3.10 or later
+			case "Harry":
+				print("Gryffindor")	
+			case _: # Catch-all
+				print("Who?")
+	
+example4()
