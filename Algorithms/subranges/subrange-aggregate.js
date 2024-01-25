cfade0a3ad323c27774ac78851f1a42060f783f6
Benjamin Wright <mail@wrightben.com>
2024-01-25 04:04:58 -0500

    gitall commit

---
+++ Algorithms/subranges/subrange-aggregate.js
@@ -0,0 +1,27 @@
+var x = "1 7";
+r = x.split(" ");
+
+// Create an array
+l = r[1]-r[0] + 1;
+a = new Array(l);
+a[0] = parseInt(r[0]);
+
+// Populate the array with the range
+for (var i = 1; i < l; i++ ) {
+	a[i] = a[i-1] + 1;
+}
+
+// List the subranges
+var sub;
+for (var i = 0; i < l; i++) {
+
+	sub = [];
+	sub.push(a[i]);
+	console.log(sub);
+		
+	for (var j = i + 1; j < l; j++) {	
+		sub.push(a[j]);
+		console.log(sub);
+	}
+	
+}
