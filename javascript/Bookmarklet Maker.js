1cc02604ee4bcede2da27f035f14a4bfbf26122a
Benjamin Wright <mail@wrightben.com>
2024-02-04 02:52:53 -0500

    Add files via upload

---
+++ javascript/Bookmarklet Maker.js
@@ -0,0 +1,11 @@
+var code = function() {
+    /*
+        Put all your bookmarklet code in
+        this function. Do not return values.
+        Finish your bookmarklet with an:
+        alert('Complete');
+    */
+
+};
+
+javascript:("+encodeURI(code.toString()+")();
