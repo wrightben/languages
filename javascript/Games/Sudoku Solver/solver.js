534f185169098e8ebafad5173a73ffd457770ec8
Benjamin Wright <mail@wrightben.com>
2024-02-05 20:54:36 -0500

    gitall commit

--- javascript/Games/Sudoku Solver/solver.js
+++ javascript/Games/Sudoku Solver/solver.js
@@ -116,36 +116,7 @@ var boxes = [
 	[61,62,63,70,71,72,79,80,81]
 ];
 
-/*
-	GRID:
-
-	1	2	3		4	5	6		7	8	9
-	10	11	12		13	14	15		16	17	18
-	19	20	21		22	23	24		25	26	27
-
-	28	29	30		31	32	33		34	35	36
-	37	38	39		40	41	42		43	44	45
-	46	47	48		49	50	51		52	53	54
-
-	55	56	57		58	59	60		61	62	63
-	64	65	66		67	68	69		70	71	72
-	73	74	75		76	77	78		79	80	81
-
-*/
-
 var puzzle = ".1...7856786.15..44...83.1....1.2.6.2...9..7...5..4.2...38..69..2..4.18.19..56.47";
-
-/*
-	STEPS (to solve):
-		1. Calculate the regexes for each dot
-		2. Seek cells with a regex and do an intersection.
-			- If it yields info, update the regexes of the connected cells.
-				- Use step strategy like Minesweeper?
-
-	STEPS (to generate):
-		1. I really don't know!
-
-*/
 
 var regexes = puzzle.split("");
 regexes.forEach(function(e,i) {
@@ -192,31 +163,5 @@ var setRegex = function (cell) { // 1-based array
 // setRegex creates the array of regexes; Grepping is now possible.
 for (var i = 1; i <= 81; i++) { setRegex(i); }
 
+// List regexes
 console.log(JSON.stringify(regexes));
-
-/*
-	See: ./filter/, ./extraneous -
-	puzzle helper.numbers
-	filter.sh
-*/
-
-
-
-
-/*	
-	PUZZLE:
-	.1...7856786.15..44...83.1....1.2.6.2...9..7...5..4.2...38..69..2..4.18.19..56.47
-	
-	GRID with Regexes:
-
-	[3,9]	[1]	[2,9]	[2,4,9]	[2]	[7]	[8]	[5]	[6]
-	[7]	[8]	[6]	[9]	[1]	[5]	[2,3]	[3]	[4]
-	[4]	[5]	[2,9]	[6]	[8]	[3]	[2,7,9]	[1]	[2,9]
-	[3,8,9]	[3,4,7]	[4,7,8,9]	[1]	[3,7]	[2]	[3,4,5,9]	[6]	[3,5,8,9]
-	[2]	[3,4,6]	[1,4,8]	[3,5]	[9]	[8]	[3,4,5]	[7]	[1,3,5]
-	[3,6,8,9]	[3,6,7]	[5]	[3,7]	[3,6,7]	[4]	[3,9]	[2]	[1,3,8,9]
-	[5]	[4,7]	[3]	[8]	[7]	[1]	[6]	[9]	[2]
-	[6]	[2]	[7]	[3]	[4]	[9]	[1]	[8]	[5]
-	[1]	[9]	[8]	[2]	[5]	[6]	[3]	[4]	[7]
-
-*/
