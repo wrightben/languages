2aa157533efc199f77168ef4714004766510677a
Benjamin Wright <mail@wrightben.com>
2024-02-26 23:43:42 -0500

    gitall commit

--- javascript/Games/Puzzle Baron/576.js
+++ javascript/Games/Puzzle Baron/576.js
@@ -46,28 +46,3 @@ re = "[34][2][134][13][123][1234][1234][12]"; // Example of puzzle clues applied
 
 ul = getFilteredList(re);
 console.log( JSON.stringify(ul), ul.length );
-
-
-
-
-
-/*
-	Logic Puzzle
-	
-	- B-A-C (The relationship between B-C is implicit.)
-	The initial regular expression filters none of the 576 list elements.
-	There are two kinds of alterations to the initial regular expression.
-		- A known relationship
-		- An exclusion
-
-	The clues tell you which alterations to make. But the language must be interpreted.
-	The clues explain that
-		- two things are related directly
-		- two or more things are not related
-		- one thing precedes another
-		
-	Question: Can B-C be extracted and does the reduction of items yield numbers for unknowns? (Is it like Sudoku?)
-	
-	Would I enjoy writing code that could parse the clues? No.
-
-*/
---
+++ javascript/Games/Puzzle Baron/README.md
@@ -0,0 +1,17 @@
+### Logic Puzzle
+	
+B-A-C (The relationship between B-C is implicit.)
+The initial regular expression filters none of the 576 list elements.
+- There are two kinds of alterations to the initial regular expression.
+	- A known relationship
+	- An exclusion
+
+The clues tell you which alterations to make. But the language must be interpreted.
+- The clues explain that
+	- two things are related directly
+	- two or more things are not related
+	- one thing precedes another
+	
+Question: Can B-C be extracted and does the reduction of items yield numbers for unknowns? (Is it like Sudoku?)
+
+Would I enjoy writing code that could parse the clues? No.
