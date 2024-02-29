a9d9a2fc4f609a834f61f23d03802f19f8e82f3f
Benjamin Wright <mail@wrightben.com>
2024-02-29 00:24:43 -0500

    gitall commit

--- javascript/Util/Util - Random Number.js
+++ javascript/Util/Util - Random Number.js
@@ -9,7 +9,7 @@ randomNumber = function(max) { // Range [1..Max]
 	randomNumber(4) - 1
 	
 	// Range 3-7
-	randomNumber(5) + 2 // 5 = span, 2 equals the displacement (from 1 to 3, range of 4 to reach 7)
+	randomNumber(5) + 2 // 5 = span, 2 equals the displacement (from 1 to 3, range of 5 to reach 7)
 	
 	// Range 3-6
 	randomNumber(4) + 2 // 4 = span, 2 equals the displacement (from 1 to 3, range of 4 to reach 6)
