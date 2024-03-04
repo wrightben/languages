e834042730efaf5828c800ad73a6466c38de3fcb
Benjamin Wright <mail@wrightben.com>
2024-03-03 23:25:25 -0500

    gitall commit

---
+++ javascript/Bookmarklet - Instagram - Right-arrow.js
@@ -0,0 +1,29 @@
+javascript: (function () {
+	
+	var x;
+	if (typeof x != "undefined") { clearInterval(x); return; }
+
+	x = setInterval(function() {
+		
+		keyCode = 39;
+		type = undefined;
+		modifiers = undefined;
+	
+		var evtName = (typeof(type) === "string") ? "key" + type : "keydown";	
+		var modifier = (typeof(modifiers) === "object") ? modifier : {};
+
+		var event = document.createEvent("HTMLEvents");
+		event.initEvent(evtName, true, false);
+		event.keyCode = keyCode;
+	
+		for (var i in modifiers) {
+			event[i] = modifiers[i];
+		}
+
+		document.dispatchEvent(event);
+		
+		console.log(x);
+	
+	}, 550);					
+
+})();
